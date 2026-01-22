89b039ea10c6ea929faf71628963803593e75054
Alexei Schwab <alexeischwab@gmail.com>
2026-01-22 21:37:01 +0000

    Rebuild plugin

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.13)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -93,7 +93,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x14\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x42\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x8e\x0f\x13\xb5\x27\
+\x00\x00\x01\x8e\x2d\xe2\xc0\xb8\
 "
 
 qt_version = [int(v) for v in QtCore.qVersion().split('.')]
